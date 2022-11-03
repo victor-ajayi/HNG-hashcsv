d96f365eea504b4b0f9e2ebd92a9e3a9f0f5680c
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2022-11-04 02:09:55 +0300

    Update hash.py to parse updated CSV

--- hash.py
+++ hash.py
@@ -14,29 +14,51 @@ def main():
 
     # Count rows in CSV
     with open(input_csv) as file:
-        row_count = sum(1 for _ in file)
+        total_rows = sum(1 for _ in file) - 1
 
     # Get CSV filename
     filename_pair = os.path.splitext(input_csv)
         
     with open(input_csv, "r") as input, open(f"{filename_pair[0]}.output.csv", "w") as output:
         reader = csv.DictReader(input)
-        headers = ["Serial Number", "Filename", "UUID", "HASH"]
+        headers = ["TEAM NAMES", "Series Number", "Filename", "Name", "Description", "Gender", "Attributes", "UUID", "Hash"]
         writer = csv.DictWriter(output, fieldnames=headers)
         writer.writeheader()
+
+        row_count = 1
+        bad_rows = 0
 
         for row in reader:
             # Create JSON structure
             dict = {
+                "format": "CHIP-0007",
                 "name": row["Filename"],
-                "description": "",
-                "series_number": row["Serial Number"],
-                "series_total": row_count,
+                "description": row["Description"],
+                "minting_tool": "TEAM NAMES",
+                "sensitive_content": False,
+                "series_number": row["Series Number"],
+                "series_total": total_rows,
+                "attributes": [],
                 "collection": {
-                    "name": "",
-                    "id": row["UUID"],
+                    "name": "Zuri NFT Tickets for Free Lunch",
+                    "id": "b774f676-c1d5-422e-beed-00ef5510c64d",
+                    "attributes": [
+                        {
+                            "type": "description",
+                            "value": "Rewards for accomplishments during HNGi9."
+                        }
+                    ]
                 }
             }
+
+            # Add attributes to JSON
+            try:
+                dict.update(parse_attributes(row["Attributes"]))
+            except Exception:
+                bad_rows += 1
+                continue
+            else:
+                row_count += 1
 
             # Save JSON by filename (if exists in row)
             try:
@@ -54,9 +76,34 @@ def main():
                 with open(f"json/{row['Filename']}.json", "rb") as json_output:
                     bytes = json_output.read()
                     hash = sha256(bytes).hexdigest()
-                    row.update({ "HASH": hash })
+                    row.update({ "Hash": hash })
                     writer.writerow(row)
 
+    print(f"{row_count} JSONs created")
+    print(f"{bad_rows} bad rows")
+
+def parse_attributes(attributes):
+    attributes = attributes.strip().split(";")
+
+    # List of attribute values in order
+    row = []
+
+    for attribute in attributes:
+        attribute_value = attribute.split(":")
+        row.append(attribute_value[1].strip())
+    
+    attribute_dict = {
+        "hair": row[0],
+        "eyes": row[1],
+        "teeth": row[2],
+        "clothing": row[3],
+        "accessories": row[4],
+        "expression": row[5],
+        "strength": row[6],
+        "weakness": row[7]
+    }
+            
+    return attribute_dict
 
 if __name__ == "__main__":
     main()
